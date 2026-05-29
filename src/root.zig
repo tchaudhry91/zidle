2588270a65033f336c1d3976df419df28a784977
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2026-05-29 14:43:49 +0530

    Make cgroup scan recursive

--- src/root.zig
+++ src/root.zig
@@ -8,10 +8,12 @@ pub fn scanCGroups(io: Io, allocator: std.mem.Allocator) !([][]const u8) {
     const cgroups = try Io.Dir.openDirAbsolute(io, "/sys/fs/cgroup/", .{ .iterate = true });
     defer cgroups.close(io);
 
-    var iter = cgroups.iterate();
-    while (try iter.next(io)) |item| {
+    var walker = try cgroups.walk(allocator);
+    defer walker.deinit();
+
+    while (try walker.next(io)) |item| {
         if (item.kind == .directory) {
-            try items.append(allocator, try allocator.dupe(u8, item.name));
+            try items.append(allocator, try allocator.dupe(u8, item.path));
         }
     }
 
